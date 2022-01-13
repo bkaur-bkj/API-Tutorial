501f093f0cddc3d18d9e8c9c7f1e74c557be2a64
Bhani <bhanikaur.juneja.che19@itbhu.ac.in>
2022-01-14 00:31:40 +0530

    basic func created

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const http = require('http')
 //here we r importing http functionality, import doesnot work here
+const app =require('./app');
 const port=process.env.port || 3000;
-const server=http.createServer();
+const server=http.createServer(app); //create server func is defined in http
+server.listen(port);
+// all this above is a simple server setup
